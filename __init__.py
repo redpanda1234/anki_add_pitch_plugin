96eaf0b2b6da1a76e94cb756207ad7051f6720ac
redpanda1234 <fkobayashi@hmc.edu>
2022-10-09 13:46:54 -0700

    add devoicing and write to fields
    whoops remove pycache

--- __init__.py
+++ __init__.py
@@ -19,6 +19,7 @@ from anki.storage import Collection
 from .util import *
 from .draw_pitch import pitch_svg
 
+
 def add_pitch_dialog():
     # environment
     collection_path = mw.col.path
@@ -26,28 +27,26 @@ def add_pitch_dialog():
 
     user_dir_path = os.path.split(collection_path)[0]
     anki_dir_path = os.path.split(user_dir_path)[0]
-    plugin_dir_path = os.path.join(anki_dir_path, 'addons21', plugin_dir_name)
+    plugin_dir_path = os.path.join(anki_dir_path, "addons21", plugin_dir_name)
 
     # load pitch dict
-    pitch_csv_path = os.path.join(plugin_dir_path, 'wadoku_pitchdb.csv')
+    pitch_csv_path = os.path.join(plugin_dir_path, "wadoku_pitchdb.csv")
     acc_dict = get_accent_dict(pitch_csv_path)
 
     # load user pitch dict if present
-    user_pitch_csv_path = os.path.join(plugin_dir_path, 'user_pitchdb.csv')
+    user_pitch_csv_path = os.path.join(plugin_dir_path, "user_pitchdb.csv")
     if os.path.isfile(user_pitch_csv_path):
-        acc_dict.update(
-            get_user_accent_dict(user_pitch_csv_path)
-        )
+        acc_dict.update(get_user_accent_dict(user_pitch_csv_path))
 
     # figure out collection structure
-    deck_id = select_deck_id('Which deck would you like to extend?')
+    deck_id = select_deck_id("Which deck would you like to extend?")
     if deck_id == None:
         return
     note_type_ids = get_note_type_ids(deck_id)
     if len(note_type_ids) > 1:
         note_type_id = select_note_type(note_type_ids)
     elif len(note_type_ids) < 1:
-        showInfo('No cards found.')
+        showInfo("No cards found.")
         return
     else:
         note_type_id = note_type_ids[0]
@@ -55,7 +54,7 @@ def add_pitch_dialog():
         return
     note_ids = get_note_ids(deck_id, note_type_id)
     if len(note_ids) == 0:
-        showInfo('No cards found.')
+        showInfo("No cards found.")
         return
     expr_idx, rdng_idx, out_idx = select_note_fields_all(note_ids[0])
     if None in [expr_idx, rdng_idx, out_idx]:
@@ -64,37 +63,50 @@ def add_pitch_dialog():
     # extend notes
     nf_lst, n_updt, n_adone, n_sfail = add_pitch(
         acc_dict, plugin_dir_name, note_ids, expr_idx, rdng_idx, out_idx
-        )
-    showInfo(('done :)\n'
-        'skipped {} already annotated notes\n'
-        'updated {} notes\n'
-        'failed to generate {} annotations\n'
-        'could not find {} expressions').format(
-            n_adone, n_updt, n_sfail, len(nf_lst)
-            )
-        )
+    )
+    showInfo(
+        (
+            "done :)\n"
+            "skipped {} already annotated notes\n"
+            "updated {} notes\n"
+            "failed to generate {} annotations\n"
+            "could not find {} expressions"
+        ).format(n_adone, n_updt, n_sfail, len(nf_lst))
+    )
+
 
 def add_pitch_dialog_user():
-    showInfo(('You can manually set pitch accent annotations when adding or'
-              ' editing cards by clicking on the \'set pitch accent\' icon'
-              ' located on the right hand side next to the text formatting'
-              ' options.'))
+    showInfo(
+        (
+            "You can manually set pitch accent annotations when adding or"
+            " editing cards by clicking on the 'set pitch accent' icon"
+            " located on the right hand side next to the text formatting"
+            " options."
+        )
+    )
+
 
 def show_custom_db_path_dialog():
     collection_path = mw.col.path
     plugin_dir_name = __name__
     user_dir_path = os.path.split(collection_path)[0]
     anki_dir_path = os.path.split(user_dir_path)[0]
-    plugin_dir_path = os.path.join(anki_dir_path, 'addons21', plugin_dir_name)
-    user_pitch_csv_path = os.path.join(plugin_dir_path, 'user_pitchdb.csv')
+    plugin_dir_path = os.path.join(anki_dir_path, "addons21", plugin_dir_name)
+    user_pitch_csv_path = os.path.join(plugin_dir_path, "user_pitchdb.csv")
 
-    showInfo(('You can extend and overwrite pitch accent patterns using the'
-              ' file "{}". The file has to be three columns (expression,'
-              ' reading, pitch accent pattern) separated by TAB characters.'
-              ''.format(user_pitch_csv_path)))
+    showInfo(
+        (
+            "You can extend and overwrite pitch accent patterns using the"
+            ' file "{}". The file has to be three columns (expression,'
+            " reading, pitch accent pattern) separated by TAB characters."
+            "".format(user_pitch_csv_path)
+        )
+    )
+
 
 def remove_pitch_dialog_user():
     return remove_pitch_dialog(user_set=True)
+
 
 def remove_pitch_dialog(user_set=False):
     # environment
@@ -103,19 +115,17 @@ def remove_pitch_dialog(user_set=False):
 
     user_dir_path = os.path.split(collection_path)[0]
     anki_dir_path = os.path.split(user_dir_path)[0]
-    plugin_dir_path = os.path.join(anki_dir_path, 'addons21', plugin_dir_name)
+    plugin_dir_path = os.path.join(anki_dir_path, "addons21", plugin_dir_name)
 
     # figure out collection structure
-    deck_id = select_deck_id(
-        'From which deck would you like to remove?'
-        )
+    deck_id = select_deck_id("From which deck would you like to remove?")
     if deck_id == None:
         return
     note_type_ids = get_note_type_ids(deck_id)
     if len(note_type_ids) > 1:
         note_type_id = select_note_type(note_type_ids)
     elif len(note_type_ids) < 1:
-        showInfo('No cards found.')
+        showInfo("No cards found.")
         return
     else:
         note_type_id = note_type_ids[0]
@@ -123,7 +133,7 @@ def remove_pitch_dialog(user_set=False):
         return
     note_ids = get_note_ids(deck_id, note_type_id)
     if len(note_ids) == 0:
-        showInfo('No cards found.')
+        showInfo("No cards found.")
         return
     del_idx = select_note_fields_del(note_ids[0])
     if del_idx == None:
@@ -131,69 +141,63 @@ def remove_pitch_dialog(user_set=False):
 
     # remove from notes
     n_adone, n_updt = remove_pitch(note_ids, del_idx, user_set)
-    showInfo(('done :)\n'
-        'skipped {} notes w/o accent annotation\n'
-        'updated {} notes').format(
-            n_adone, n_updt
-            )
-        )
+    showInfo(
+        (
+            "done :)\n" "skipped {} notes w/o accent annotation\n" "updated {} notes"
+        ).format(n_adone, n_updt)
+    )
+
 
 def set_pitch_dialog(editor):
-    if editor.web.editor.currentField is None:
-        showInfo('A field needs to be selected')
-        return
+    # get user input from card
+    pitch_reading = editor.note["pitch_reading"]
+    pitch_pattern = editor.note["pitch_pattern"]
 
-    # get user input
-    hira, hira_succeeded = getText('Enter the reading to be set. (Example: はな)')
-    if not hira_succeeded:
-        return
-    
-    LH_patt, LH_patt_succeeded = getText(
-        ('Enter the pitch accent pattern as a sequence of \'H\'s and \'L\'s. '
-        '(Example: LHL)')
-    )
-    if not LH_patt_succeeded:
-        return
-
-    # get note data
-    data = [
-        (fld, editor.mw.col.media.escapeImages(val))
-        for fld, val in editor.note.items()
+    # get note data as a list of tuples of the form (field name, contents)
+    note_data = [
+        (fld, editor.mw.col.media.escapeImages(val)) for fld, val in editor.note.items()
     ]
 
-    # remove existing patt
+    # Figure out the index of the field named "pitch_image"
+    pitch_image_ind = 0
+    for (i, datum) in enumerate(note_data):
+        if datum[0] == "pitch_image":
+            pitch_image_ind = i
+            break
+
+    # remove existing accent pattern
     acc_patt = re.compile(
-        r'<!-- (user_)?accent_start -->.+<!-- (user_)?accent_end -->',
-        re.S
+        r"<!-- (user_)?accent_start -->.+<!-- (user_)?accent_end -->", re.S
     )
-    old_field_val = data[editor.web.editor.currentField][1]
-    old_field_val_clean = re.sub(acc_patt, '', old_field_val)
+    old_field_val = note_data[pitch_image_ind][1]
+    old_field_val_clean = re.sub(acc_patt, "", old_field_val)
 
     # generate SVG
-    svg = pitch_svg(hira, LH_patt)
+    svg = pitch_svg(pitch_reading, pitch_pattern)
     if len(old_field_val_clean) > 0:
-        separator = '<br><hr><br>'
+        separator = "<br><hr><br>"
     else:
-        separator = ''
-    new_field_val = (
-        '{}<!-- user_accent_start -->{}{}<!-- user_accent_end -->'
-        ).format(old_field_val_clean, separator, svg)
-    if hira == '' and LH_patt == '':
+        separator = ""
+    new_field_val = ("{}<!-- user_accent_start -->{}{}<!-- user_accent_end -->").format(
+        old_field_val_clean, separator, svg
+    )
+    if pitch_reading == "" and pitch_pattern == "":
         new_field_val = old_field_val_clean
 
     # add new patt
-    data[editor.web.editor.currentField] = (
-        data[editor.web.editor.currentField][0],       # leave field name as is
-        new_field_val                                  # update field value
+    note_data[pitch_image_ind] = (
+        note_data[pitch_image_ind][0],  # leave field name as is
+        new_field_val,  # update field value
     )
-    js = 'setFields(%s); setFonts(%s); focusField(%s); setNoteId(%s)' % (
-        json.dumps(data),
+    js = "setFields(%s); setFonts(%s); focusField(%s); setNoteId(%s)" % (
+        json.dumps(note_data),
         json.dumps(editor.fonts()),
-        json.dumps(editor.web.editor.currentField),
-        json.dumps(editor.note.id)
+        json.dumps(pitch_image_ind),
+        json.dumps(editor.note.id),
     )
     js = gui_hooks.editor_will_load_note(js, editor.note, editor)
     editor.web.eval(js)
+
 
 def addPitchButton(buttons, editor):
     # environment
@@ -202,22 +206,20 @@ def addPitchButton(buttons, editor):
 
     user_dir_path = os.path.split(collection_path)[0]
     anki_dir_path = os.path.split(user_dir_path)[0]
-    plugin_dir_path = os.path.join(anki_dir_path, 'addons21', plugin_dir_name)
-    icon_path = os.path.join(plugin_dir_path, 'icon.png')
+    plugin_dir_path = os.path.join(anki_dir_path, "addons21", plugin_dir_name)
+    icon_path = os.path.join(plugin_dir_path, "icon.png")
 
-    btn = editor.addButton(icon_path,
-                         'foo',
-                         set_pitch_dialog,
-                         tip='set pitch accent')
+    btn = editor.addButton(icon_path, "foo", set_pitch_dialog, tip="set pitch accent")
     buttons.append(btn)
 
+
 # add menu items
-pa_menu = QMenu('Pitch Accent', mw)
-pa_menu_add = pa_menu.addAction('bulk add')
-pa_menu_remove = pa_menu.addAction('bulk remove')
-pa_menu_add_user = pa_menu.addAction('manually add/edit/remove')
-pa_menu_remove_user = pa_menu.addAction('remove all manually set')
-pa_menu_custom_db_path = pa_menu.addAction('show custom DB path')
+pa_menu = QMenu("Pitch Accent", mw)
+pa_menu_add = pa_menu.addAction("bulk add")
+pa_menu_remove = pa_menu.addAction("bulk remove")
+pa_menu_add_user = pa_menu.addAction("manually add/edit/remove")
+pa_menu_remove_user = pa_menu.addAction("remove all manually set")
+pa_menu_custom_db_path = pa_menu.addAction("show custom DB path")
 # add triggers
 pa_menu_add.triggered.connect(add_pitch_dialog)
 pa_menu_remove.triggered.connect(remove_pitch_dialog)
--- draw_pitch.py
+++ draw_pitch.py
@@ -1,109 +1,157 @@
 import sys
 
+
 def hira_to_mora(hira):
-    """ Example:
-            in:  'しゅんかしゅうとう'
-            out: ['しゅ', 'ん', 'か', 'しゅ', 'う', 'と', 'う']
+    """Example:
+    in:  'しゅんかしゅうとう'
+    out: ['しゅ', 'ん', 'か', 'しゅ', 'う', 'と', 'う']
     """
 
     mora_arr = []
-    combiners = ['ゃ', 'ゅ', 'ょ', 'ぁ', 'ぃ', 'ぅ', 'ぇ', 'ぉ',
-                 'ャ', 'ュ', 'ョ', 'ァ', 'ィ', 'ゥ', 'ェ', 'ォ']
+    combiners = [
+        "ゃ",
+        "ゅ",
+        "ょ",
+        "ぁ",
+        "ぃ",
+        "ぅ",
+        "ぇ",
+        "ぉ",
+        "ャ",
+        "ュ",
+        "ョ",
+        "ァ",
+        "ィ",
+        "ゥ",
+        "ェ",
+        "ォ",
+    ]
 
     i = 0
     while i < len(hira):
-        if i+1<len(hira) and hira[i+1] in combiners:
-            mora_arr.append('{}{}'.format(hira[i], hira[i+1]))
+        if i + 1 < len(hira) and hira[i + 1] in combiners:
+            mora_arr.append("{}{}".format(hira[i], hira[i + 1]))
             i += 2
         else:
             mora_arr.append(hira[i])
             i += 1
     return mora_arr
 
-def circle(x, y, o=False):
-    r = ('<circle r="5" cx="{}" cy="{}" style="opacity:1;fill:#000;" />'
+
+def circle(x, y, devoiced=False, o=False):
+    if not devoiced:
+        r = ('<circle r="5" cx="{}" cy="{}" style="opacity:1;fill:#000;" />').format(
+            x, y
+        )
+
+        if o:
+            r += (
+                '<circle r="3.25" cx="{}" cy="{}" style="opacity:1;fill:#fff;"' "/>"
+            ).format(x, y)
+    else:
+        assert not o
+        r = (
+            '<circle r="5" cx="{}" cy="{}" stroke="black" stroke-dasharray="2" style="opacity:1;fill:#EBEBEB;" />'
         ).format(x, y)
-    if o:
-        r += ('<circle r="3.25" cx="{}" cy="{}" style="opacity:1;fill:#fff;"'
-              '/>').format(x, y)
     return r
 
-def text(x, mora):
+
+def text(x, mora, devoiced=False):
     # letter positioning tested with Noto Sans CJK JP
+    color = "#000"
+    if devoiced:
+        color = "#CDCDCD"
     if len(mora) == 1:
-        return ('<text x="{}" y="67.5" style="font-size:20px;font-family:sans-'
-                'serif;fill:#000;">{}</text>').format(x, mora)
+        return (
+            '<text x="{}" y="67.5" style="font-size:20px;font-family:sans-'
+            "serif;fill:" + color + ';">{}</text>'
+        ).format(x, mora)
     else:
-        return ('<text x="{}" y="67.5" style="font-size:20px;font-family:sans-'
-                'serif;fill:#000;">{}</text><text x="{}" y="67.5" style="font-'
-                'size:14px;font-family:sans-serif;fill:#000;">{}</text>'
-               ).format(x-5, mora[0], x+12, mora[1])
+        return (
+            '<text x="{}" y="67.5" style="font-size:20px;font-family:sans-'
+            "serif;fill:" + color + ';">{}</text><text x="{}" y="67.5" style="font-'
+            "size:14px;font-family:sans-serif;fill:" + color + ';">{}</text>'
+        ).format(x - 5, mora[0], x + 12, mora[1])
+
 
 def path(x, y, typ, step_width):
-    if typ == 's':  # straight
-        delta = '{},0'.format(step_width)
-    elif typ == 'u':  # up
-        delta = '{},-25'.format(step_width)
-    elif typ == 'd':  # down
-        delta = '{},25'.format(step_width)
-    return ('<path d="m {},{} {}" style="fill:none;stroke:#000;stroke-width'
-            ':1.5;" />').format(x, y, delta)
+    if typ == "s":  # straight
+        delta = "{},0".format(step_width)
+    elif typ == "u":  # up
+        delta = "{},-25".format(step_width)
+    elif typ == "d":  # down
+        delta = "{},25".format(step_width)
+    return (
+        '<path d="m {},{} {}" style="fill:none;stroke:#000;stroke-width' ':1.5;" />'
+    ).format(x, y, delta)
+
 
 def pitch_svg(word, patt, silent=False):
-    """ Draw pitch accent patterns in SVG
+    """Draw pitch accent patterns in SVG
 
-        Examples:
-            はし HLL (箸)
-            はし LHL (橋)
-            はし LHH (端)
+    Examples:
+        はし HLL (箸)
+        はし LHL (橋)
+        はし LHH (端)
     """
 
     mora = hira_to_mora(word)
 
     if len(patt) - len(mora) != 1 and not silent:
-        print(('pattern should be number of morae + 1 (got: {}, {})'
-              ).format(word, patt))
+        print(
+            ("pattern should be number of morae + 1 (got: {}, {})").format(word, patt)
+        )
     positions = max(len(mora), len(patt))
     step_width = 35
     margin_lr = 16
-    svg_width = max(0, ((positions-1) * step_width) + (margin_lr*2))
+    svg_width = max(0, ((positions - 1) * step_width) + (margin_lr * 2))
 
-    svg = ('<svg class="pitch" width="{0}px" height="75px" viewBox="0 0 {0} 75'
-           '">').format(svg_width)
+    svg = (
+        '<svg class="pitch" width="{0}px" height="75px" viewBox="0 0 {0} 75' '">'
+    ).format(svg_width)
 
-    chars = ''
+    chars = ""
     for pos, mor in enumerate(mora):
         x_center = margin_lr + (pos * step_width)
-        chars += text(x_center-11, mor)
+        devoiced = (pos < len(patt)) and (patt[pos] in ["h", "l"])
+        chars += text(x_center - 11, mor, devoiced=devoiced)
 
-    circles = ''
-    paths = ''
+    circles = ""
+    paths = ""
     for pos, accent in enumerate(patt):
         x_center = margin_lr + (pos * step_width)
-        if accent in ['H', 'h', '1', '2']:
+        if accent in ["H", "h", "1", "2"]:
             y_center = 5
-        elif accent in ['L', 'l', '0']:
+        elif accent in ["L", "l", "0"]:
             y_center = 30
-        circles += circle(x_center, y_center, pos>=len(mora))
+
+        devoiced = (pos < len(mora)) and (accent in ["h", "l"])
+
+        circles += circle(x_center, y_center, devoiced=devoiced, o=pos >= len(mora))
         if pos > 0:
             if prev_center[1] == y_center:
-                path_typ = 's'
+                path_typ = "s"
             elif prev_center[1] < y_center:
-                path_typ = 'd'
+                path_typ = "d"
             elif prev_center[1] > y_center:
-                path_typ = 'u'
+                path_typ = "u"
             paths += path(prev_center[0], prev_center[1], path_typ, step_width)
         prev_center = (x_center, y_center)
+
+        # Don't bother drawing more than one trailing pitch guide
+        if pos >= len(mora):
+            break
 
     svg += chars
     svg += paths
     svg += circles
-    svg += '</svg>'
+    svg += "</svg>"
 
     return svg
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print('usage: python3 draw_pitch.py <word> <patt>')
+        print("usage: python3 draw_pitch.py <word> <patt>")
         sys.exit()
     print(pitch_svg(sys.argv[1], sys.argv[2], silent=True))
